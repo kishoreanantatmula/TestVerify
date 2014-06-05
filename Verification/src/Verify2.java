e823ba7efa84d4e75d89a78ca276a0885baa124d
kishoreanantatmula <anantha6445@gmail.com>
2014-06-05 17:17:51 +0530

    Revert "Commting correct Program name"
    This reverts commit cd42241ea2651ccee19842f8bed8ddb29383c602.

--- Verification/src/Verify2.java
+++ Verification/src/Verify2.java
@@ -6,10 +6,6 @@ public class Verify2 {
 		
 		System.out.println("Verify 3333333");
 		
-		System.out.println("THis is Verify2 not verify 3");
-		
-		System.out.println("Verify 2222222222");
-		
 	}
 	
 }
